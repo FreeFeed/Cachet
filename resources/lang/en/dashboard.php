4311eeaeeb7778ec4f00dc93a95460ded90e9f7a
James Brooks <jbrooksuk@me.com>
2016-02-09 19:09:30 +0000

    Display what subscriptions (if any) a subscriber has

--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -140,11 +140,13 @@ return [
     ],
     // Subscribers
     'subscribers' => [
-        'subscribers'  => 'Subscribers',
-        'description'  => 'Subscribers will receive email updates when incidents are created.',
-        'verified'     => 'Verified',
-        'not_verified' => 'Not verified',
-        'add'          => [
+        'subscribers'      => 'Subscribers',
+        'description'      => 'Subscribers will receive email updates when incidents are created.',
+        'verified'         => 'Verified',
+        'not_verified'     => 'Not verified',
+        'subscriber'       => ':email, subscribed :date',
+        'no_subscriptions' => 'Subscribed to all updates',
+        'add'              => [
             'title'   => 'Add a new subscriber',
             'success' => 'Subscriber has been added!',
             'failure' => 'The subscriber could not be added. Please try again.',
